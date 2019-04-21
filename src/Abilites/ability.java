5841614780cb224d3a1aaf6237eb6b08a4c21977
IceCereal <ns_raghav@hotmail.com>
2019-04-21 18:23:08 +0530

    create ability

---
+++ src/Abilites/ability.java
@@ -0,0 +1,17 @@
+package io.github.IceCereal.src.Abilites;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ability{
+	Map<String, Integer> Abilities = new HashMap<String, Integer>();	
+	/*
+		Extra Abilites that the player could have: defaults [None]
+		Every Ability created can have  called Ability_Reductor:
+			Does Ability reduce every round: defaults [0]
+		Ex:
+			Mana: 200
+			Mana_Reductor: 3
+	*/
+}
+
