be13ccb470b1f1eb8614eadd1cc8534b8f3eb0dc
IceCereal <ns_raghav@hotmail.com>
2019-04-24 20:21:41 +0530

    Create Room.java

---
+++ src/RoomUtils/Room/Room.java
@@ -0,0 +1,12 @@
+package io.github.IceCereal.src.RoomUtils.Room;
+
+public class Room{
+	private int x, y;	// The X, Y coordinate of the room. Internal property set by Core Engine
+
+	private String name;	// Room name: defaults ["Unknown"]
+	
+	private String description_long;	// Long Description of Room: defaults ["There seems to be nothing special about <RoomName>"]
+	private String description_short;	// Short Description of Room: defaults ["There seems to be nothing special about <RoomName>"]
+
+	// TODO: implement an array of Gainbjects
+}
