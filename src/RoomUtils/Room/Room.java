d3c2bb61947471dee12dd29498bd37af932e1d2a
IceCereal <ns_raghav@hotmail.com>
2019-04-24 20:28:41 +0530

    Add Constructor

--- src/RoomUtils/Room/Room.java
+++ src/RoomUtils/Room/Room.java
@@ -9,4 +9,13 @@ public class Room{
 	private String description_short;	// Short Description of Room: defaults ["There seems to be nothing special here"]
 
 	// TODO: implement an array of Gainbjects
+
+	public Room(int sentX, int sentY){
+		x = sentX;
+		y = sentY;
+
+		name = "Unknown";
+		description_long = "There seems to be nothing special about here";
+		description_short = "There seems to be nothing special about here";
+	}
 }
