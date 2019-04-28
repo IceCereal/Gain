d895d2a125e519ef482b2e1cc8d32a53539fd154
IceCereal <ns_raghav@hotmail.com>
2019-04-28 13:19:38 +0530

    add setters

--- src/NPC/nonPlayer.java
+++ src/NPC/nonPlayer.java
@@ -18,4 +18,32 @@ class NPC{
 	*/
 
 	int attackingLevel; // Level at which the NPC attacks: defaults [0] (range 0 - 9)
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setDescription_long(String description_long) {
+		this.description_long = description_long;
+	}
+
+	public void setDescription_short(String description_short) {
+		this.description_short = description_short;
+	}
+
+	public void setHealthPoints(int healthPoints) {
+		this.healthPoints = healthPoints;
+	}
+
+	public void setHealthPoints_Reductor(int healthPoints_Reductor) {
+		this.healthPoints_Reductor = healthPoints_Reductor;
+	}
+
+	public void setTypeNPC(int typeNPC) {
+		this.typeNPC = typeNPC;
+	}
+
+	public void setAttackingLevel(int attackingLevel) {
+		this.attackingLevel = attackingLevel;
+	}
 }
