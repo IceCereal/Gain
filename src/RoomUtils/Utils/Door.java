f45d6d75b2dd547b6ddc2425e244e331e34fd3f5
IceCereal <ns_raghav@hotmail.com>
2019-04-28 12:09:10 +0530

    Create Vars

--- src/RoomUtils/Utils/Door.java
+++ src/RoomUtils/Utils/Door.java
@@ -3,3 +3,10 @@ package io.github.IceCereal.src.RoomUtils.Utils;
 import java.util.ArrayList;
 import java.util.List;
 
+class Door{
+	private List<Integer> coordinatesFrom; 	// Door opens from these coords
+	private List<Integer> coordinatesTo; 	// Door opens to these coords
+
+	Bool key_required; 	// Boolean that checks whether the key is required
+	int key_id; 	// If key_required: True, key_id represents the key id for the door
+}
