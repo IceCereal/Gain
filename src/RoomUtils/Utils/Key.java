7437dd1f005825952ca4cf3de69b31a377ecec56
IceCereal <ns_raghav@hotmail.com>
2019-04-29 19:52:41 +0530

    add constructor

--- src/RoomUtils/Utils/Key.java
+++ src/RoomUtils/Utils/Key.java
@@ -3,4 +3,9 @@ package io.github.IceCereal.src.RoomUtils.Utils;
 class Key{
 	private int key_id; 	// Engine set key_id
 	private Boolean hasKey; 	// Not sure if this will be useful, but I'll keep it here for now
+
+	public Key(){
+		key_id = 0;
+		hasKey = false;
+	}
 }
